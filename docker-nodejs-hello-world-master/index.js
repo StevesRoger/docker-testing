989cf00f709e2ec55f6b74350612a4a1121ae635
EC2 Default User <ec2-user@ip-10-0-1-13.ap-southeast-1.compute.internal>
2021-07-23 03:55:53 +0000

    init

---
+++ docker-nodejs-hello-world-master/index.js
@@ -0,0 +1,20 @@
+var express = require('express')
+var app = express();
+const os = require("os")
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  var id = os.hostname()
+  var obj = { msg: 'Hello', container_id: id }
+  var json = JSON.stringify(obj)
+  response.writeHead(200, {'Content-Type': 'application/json'})
+  response.write(json)
+  response.end()
+})
+
+app.listen(app.get('port'), function() {	
+  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("I'm in container:", os.hostname())
+})
