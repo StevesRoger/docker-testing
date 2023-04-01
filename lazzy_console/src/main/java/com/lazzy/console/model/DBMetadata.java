2a13ede63fa958b174f97863a92ace87722fe427
chheng <kimchheng101@gmail.com>
2023-04-01 17:58:00 +0700

    update java console

---
+++ lazzy_console/src/main/java/com/lazzy/console/model/DBMetadata.java
@@ -0,0 +1,76 @@
+package com.lazzy.console.model;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Created: chheng
+ * Date: 01-Apr-2023 Sat
+ * Time: 16:25
+ */
+public class DatabaseMetadata {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+    private String databaseName;
+
+    public DatabaseMetadata() {
+    }
+
+    public DatabaseMetadata(String host, int port, String username, String password, String databaseName) {
+        this.host = host;
+        this.port = port;
+        this.username = username;
+        this.password = password;
+        this.databaseName = databaseName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDatabaseName() {
+        return databaseName;
+    }
+
+    public void setDatabaseName(String databaseName) {
+        this.databaseName = databaseName;
+    }
+
+    public boolean isValid(){
+        return StringUtils.isNotEmpty(host)
+                && port>0
+                && StringUtils.isNotEmpty(username)
+                && StringUtils.isNotEmpty(password)
+                && StringUtils.isNotEmpty(databaseName);
+    }
+}
---
+++ lazzy_console/src/main/java/com/lazzy/console/model/JNTAreaResponse.java
@@ -0,0 +1,9 @@
+package com.lazzy.console.model;
+
+/**
+ * Created: chheng
+ * Date: 01-Apr-2023 Sat
+ * Time: 17:03
+ */
+public class JNTAreaResponse {
+}
