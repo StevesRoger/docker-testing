fd8723229965d779dfd2f06c06182d3ed2d26a15
Kimchheng Kao <kimchheng.kao@wingmoney.com>
2023-04-11 16:35:03 +0700

    update node js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,3 +1,16 @@
+process.on("uncaughtException", (error) => {
+  console.log("Oh my god, something terrible happened: ", error);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (error, promise) => {
+  console.log(
+    " Oh Lord! We forgot to handle a promise rejection here: ",
+    promise
+  );
+  console.log(" The error was: ", error);
+});
+
 var express = require("express");
 var app = express();
 const os = require("os");
@@ -11,47 +24,77 @@ app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
 
-app.get("/", function (req, res) {
-  var id = os.hostname();
-  var obj = { msg: "Hello World", container_id: id };
+app.get("/", (req, res) => {
+  var obj = { msg: "Hello World", container_id: os.hostname() };
   var json = JSON.stringify(obj);
   res.writeHead(200, { "Content-Type": "application/json" });
   res.write(json);
   res.end();
 });
 
-app.post("/translate", async function (req, res) {
-  console.log("===>>> request body ", req.body);
-  var words = req.body["words"];
-  var strWords = "";
-  for (var i = 0; i < words.length; i++) {
-    strWords += words[i] + ",";
-  }
-  strWords = strWords.slice(0, -1);
-  console.log("===>>> words ", strWords);
-  var body = {};
-  var status = 200;
+app.post("/translate", async (req, res) => {
   try {
-    var data = await translator.translate(strWords, {
-      to: "en",
-      fetchOptions: { agent },
-    });
-    console.log("===>>> translate ", data.text);
-    var id = os.hostname();
+    console.log("===>>> request body ", req.body);
+    const chunkSize = 200;
+    const reg = new RegExp("\\.", "g");
+    var words = req.body["words"];
+    var translateData = [];
+    var body = {};
+    var status = 200;
+    for (let i = 0; i < words.length; i += chunkSize) {
+      console.log("===>>> chunk array words translate by", chunkSize);
+      const chunk = words.slice(i, i + chunkSize);
+      var strWord = "";
+      for (var j = 0; j < chunk.length; j++) {
+        strWord += chunk[j].replace(/\s/g, "").trim() + ", ";
+      }
+      strWord = strWord.slice(0, -2).trim();
+      console.log("===>>> words ", strWord);
+      const result = await requestTranslate(strWord);
+      translateData = translateData.concat(
+        result
+          .replace(reg, ",")
+          .split(",")
+          .filter((v) => {
+            return (
+              v.toLowerCase().trim() !== "city" &&
+              v.toLowerCase().trim() !== "district" &&
+              v.toLowerCase().trim() !== "commune" &&
+              v.toLowerCase().trim() !== "province" &&
+              v.toLowerCase().trim() !== "" &&
+              !/^-?\d+$/.test(v.toLowerCase().trim())
+            );
+          })
+          .map((v) => v.trim().toLowerCase())
+      );
+    }
     body = {
-      translate: data.text.split(",").map((v) => v.trim().toLowerCase()),
-      container_id: id,
+      translate: translateData,
+      container_id: os.hostname(),
     };
   } catch (e) {
     console.log(e);
     body = e;
+    status = 400;
   }
   res.writeHead(status, { "Content-Type": "application/json" });
   res.write(JSON.stringify(body));
   res.end();
 });
 
-app.listen(app.get("port"), function () {
+async function requestTranslate(strWord) {
+  console.log(
+    "===>>> agent proxy " + agent.proxy.host + ":" + agent.proxy.port
+  );
+  var data = await translator.translate(strWord.replace(/\s/g, ""), {
+    to: "en",
+    fetchOptions: { agent },
+  });
+  console.log("===>>> translate ", data.text);
+  return data.text;
+}
+
+app.listen(app.get("port"), () => {
   console.log("Node app is running at localhost:" + app.get("port"));
   console.log("I'm in container:", os.hostname());
 });
